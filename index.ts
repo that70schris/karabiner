55662555ab7d9a6cf5ad0b8e3347a5f05eb4bf8e
Chris Bailey <chris@bailey.mx>
2024-08-23 13:38:33 -0400

    move rules to separate file

---
+++ index.ts
@@ -0,0 +1,79 @@
+import { writeFileSync } from 'fs';
+import './extensions';
+import rules from './rules';
+import { KarabinerRule } from './types';
+import { createHyperSubLayers } from './utils';
+
+writeFileSync(
+  'karabiner.json',
+  JSON.stringify(
+    {
+      global: {
+        show_in_menu_bar: false,
+      },
+      profiles: [
+        {
+          name: 'Default',
+          complex_modifications: {
+            rules: [
+              {
+                description: 'Hyper Key (⌃⌥⇧⌘)',
+                manipulators: [
+                  {
+                    description: 'Caps Lock -> Hyper Key',
+                    type: 'basic',
+                    from: {
+                      key_code: 'caps_lock',
+                      modifiers: {
+                        optional: ['any'],
+                      },
+                    },
+                    to: [
+                      {
+                        set_variable: {
+                          name: 'hyper',
+                          value: 1,
+                        },
+                      },
+                    ],
+                    to_after_key_up: [
+                      {
+                        set_variable: {
+                          name: 'hyper',
+                          value: 0,
+                        },
+                      },
+                    ],
+                    to_if_alone: [
+                      {
+                        key_code: 'escape',
+                      },
+                    ],
+                  },
+                ],
+              },
+              // {
+              //   type: 'basic',
+              //   description: 'Disable CMD + Tab to force Hyper Key usage',
+              //   from: {
+              //     key_code: 'tab',
+              //     modifiers: {
+              //       mandatory: ['left_command'],
+              //     },
+              //   },
+              //   to: [
+              //     {
+              //       key_code: 'tab',
+              //     },
+              //   ],
+              // },
+              ...createHyperSubLayers(rules),
+            ] as KarabinerRule[],
+          },
+        },
+      ],
+    },
+    null,
+    2,
+  ),
+);
--- rules.ts
+++ rules.ts
@@ -1,107 +1,28 @@
-import { writeFileSync } from 'fs';
-import { KarabinerRule } from './types';
-import { createHyperSubLayers } from './utils';
+import { HyperKeySublayer, KeyCode, LayerCommand } from './types';
 
-const rules: KarabinerRule[] = [
-  // Define the Hyper key itself
+export default {
+  // o = "Open" applications
+  o: {
+    a: 'Arc'.app,
+    c: 'Calendar'.app,
+    d: 'Docker'.app,
+    e: 'Mail'.app,
+    f: 'Finder'.app,
+    i: 'Messages'.app, // 'i'Message
+    n: 'Notes'.app,
+    s: 'Slack'.app,
+    t: 'Warp'.app,
+    v: 'Visual Studio Code'.app,
+    w: 'Weather'.app,
+    x: 'Xcode'.app,
+    z: 'zoom.us'.app,
+  },
 
-  ...createHyperSubLayers({
-    // o = "Open" applications
-    o: {
-      a: 'Arc'.app,
-      c: 'Calendar'.app,
-      d: 'Docker'.app,
-      e: 'Mail'.app,
-      f: 'Finder'.app,
-      i: 'Messages'.app, // 'i'Message
-      n: 'Notes'.app,
-      s: 'Slack'.app,
-      t: 'Warp'.app,
-      v: 'Visual Studio Code'.app,
-      w: 'Weather'.app,
-      x: 'Xcode'.app,
-      z: 'zoom.us'.app,
-    },
-
-    // s = "System"
-    s: {
-      backslash: `raycast://extensions/raycast/system/toggle-system-appearance`
-        .open,
-    },
-  }),
-];
-
-writeFileSync(
-  'karabiner.json',
-  JSON.stringify(
-    {
-      global: {
-        show_in_menu_bar: false,
-      },
-      profiles: [
-        {
-          name: 'Default',
-          complex_modifications: {
-            rules: (
-              [
-                {
-                  description: 'Hyper Key (⌃⌥⇧⌘)',
-                  manipulators: [
-                    {
-                      description: 'Caps Lock -> Hyper Key',
-                      type: 'basic',
-                      from: {
-                        key_code: 'caps_lock',
-                        modifiers: {
-                          optional: ['any'],
-                        },
-                      },
-                      to: [
-                        {
-                          set_variable: {
-                            name: 'hyper',
-                            value: 1,
-                          },
-                        },
-                      ],
-                      to_after_key_up: [
-                        {
-                          set_variable: {
-                            name: 'hyper',
-                            value: 0,
-                          },
-                        },
-                      ],
-                      to_if_alone: [
-                        {
-                          key_code: 'escape',
-                        },
-                      ],
-                    },
-                  ],
-                },
-                // {
-                //   type: 'basic',
-                //   description: 'Disable CMD + Tab to force Hyper Key usage',
-                //   from: {
-                //     key_code: 'tab',
-                //     modifiers: {
-                //       mandatory: ['left_command'],
-                //     },
-                //   },
-                //   to: [
-                //     {
-                //       key_code: 'tab',
-                //     },
-                //   ],
-                // },
-              ] as KarabinerRule[]
-            ).concat(rules),
-          },
-        },
-      ],
-    },
-    null,
-    2,
-  ),
-);
+  // s = "System"
+  s: {
+    backslash: `raycast://extensions/raycast/system/toggle-system-appearance`
+      .open,
+  },
+} satisfies {
+  [key_code in KeyCode]?: HyperKeySublayer | LayerCommand;
+};
